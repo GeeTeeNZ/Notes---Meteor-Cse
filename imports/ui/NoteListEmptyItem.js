488f96f8553e245bd5a4ad11716f06cad1dcef19
Graeme Thorpe <nzgeetee@gmail.com>
2017-06-04 00:09:42 +1200

    Style note list

--- imports/ui/NoteListEmptyItem.js
+++ imports/ui/NoteListEmptyItem.js
@@ -2,10 +2,7 @@ import React from 'react';
 
 const NoteListEmptyItem = () => {
   return (
-    <div>
-      <h4>No Notes found!!</h4>
-      <h5>Add a new note to begin</h5>
-    </div>
+    <p className='empty-item'>Add a new note to begin</p>
   );
 }
 
